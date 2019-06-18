98968c10fa03f328e47569119d2a5cb48c554392
SVNMLR <sv3n-moeller@web.de>
2019-06-18 13:15:43 +0200

    Update 5 - Codestruktur geändert nach JPM Version - Umstellung auf .jsx

--- dist/script.jsx
+++ dist/script.jsx
@@ -8,11 +8,204 @@
 // Version : 0.1							               //
 /////////////////////////////////////////////*/
 
+var contact_deleted = false;
+var contact_make_new = false;
+var contact_edit = false;
+
+class App extends React.Component {
+
+  constructor(props) {
+    //Default Werte eistellen
+     super(props);
+     this.state = {
+       contacts: [
+        {
+          name: 'Mary Margaret Blanchard',
+          age: 12,
+          phone: '555-1234',
+          email: 'snowwhite@ouatmail.com',
+          address: '945 N. Storybrook Ln',
+          image: 'http://www.starwarshelmets.com/2012/Sideshow_weathered3PO04.jpg',
+          isActive: true 
+        },
+        
+        {
+          name: 'David Nolan',
+          age: 22,
+          phone: '555-9876',
+          email: 'princecharming@ouatmail.com',
+          address: '945 N. Storybrook Ln',
+          image: 'http://cdn.playbuzz.com/cdn/e6516fc7-27c5-4798-8097-3553691017a2/5cab9b6e-6a4c-4bda-90fe-c8af89826b03.jpg',
+          isActive: false 
+        },
+        
+        {
+          name: 'Emma Swan',
+          age: 34,
+          phone: '555-2345',
+          email: 'thesavior@ouatmail.com',
+          address: '693 N. Storybrook Ln',
+          image: 'https://a.wattpad.com/cover/4106257-368-k161010.jpg',
+          isActive: false 
+        },
+        
+        {
+          name: 'Regina Mills',
+          age: 55,
+          phone: '555-6667',
+          email: 'evilqueen@ouatmail.com',
+          address: '1313 E. Mayor St',
+          image: 'https://s-media-cache-ak0.pinimg.com/originals/11/d3/e9/11d3e91be6df715f88d543374c039e1d.jpg',
+          isActive: false 
+        },
+        
+        {
+          name: 'Mr. Gold',
+          age: 12,
+          phone: '555-4653',
+          email: 'rumplestiltskin@ouatmail.com',
+          address: '101 N. Main St',
+          image: 'https://captainawkwarddotcom.files.wordpress.com/2014/02/darth-vader-original_0.jpg',
+          isActive: false 
+        }
+       ],
+        contact_deleted: false,
+        contact_make_new: false,
+        contact_edit: false,
+     };
+   }
+
+   
+     //Update Funktionen für die ezeinen Elemente erzeugen
+     updateName(event) {
+       this.setState({ name: event.target.value });
+     }
+
+     updateAge(event) {
+       this.setState({ age: event.target.value });
+     }
+
+     updatePhone(event){
+       this.setState({ phone: event.target.value});
+     }
+
+     updateMail(event) {
+       this.setState({ email: event.target.value });
+     }
+
+     updateAddress(event) {
+      this.setState({ address: event.target.value });
+    }
+
+    updateImage(event) {
+      this.setState({ image: event.target.value });
+    }
+
+     addKontakt(event) {
+       contact_edit = true;
+       this.setState({contact_deleted: contact_edit});
+     }
+
+     saveKontakt(event) {
+      contact_edit = false;
+      var newContacts = this.state.contacts.concat({name: this.state.name, age: this.state.age, phone: this.state.phone, email: this.state.email, address: this.state.address, image: this.state.image, isActive: this.state.isActive});
+      this.setState({contacts: newContacts});
+    }
+
+     editKontakt(event){
+      var newContacts = this.state.contacts.concat({name: this.state.name, age: this.state.age, phone: this.state.phone, email: this.state.email, address: this.state.address, image: this.state.image, isActive: this.state.isActive});
+     }
+
+     delKontakt(email) {
+      var newContacts = this.state.contacts.filter(value => value.email != email);
+      this.setState({contacts: newContacts});
+     }
+
+     cancel(event){
+      contact_edit = false;
+      this.setState({contact_deleted: contact_edit});
+     }
+
+
+//HTML Ausgeben um Formular Anzeigen
+  render() {
+    if(contact_edit == true)
+    {
+      return (
+        //Fragment Anzeigen um Abzugrenzen
+      <React.Fragment>
+      <h2>Neuen Kontakt erstellen</h2>
+  
+      <label>Name: <input onChange={this.updateName.bind(this)} type="text"/></label>
+      {<br></br>}
+      <label>Alter: <input onChange={this.updateAge.bind(this)} type="number" min="1"/></label>
+      {<br></br>}
+      <label>Telefon: <input onChange={this.updatePhone.bind(this)} type="text"/></label>
+      {<br></br>}
+      <label>E-Mail-Adresse: <input onChange={this.updateMail.bind(this)} type="mail"/></label>
+      {<br></br>}
+      <label>Adresse: <input onChange={this.updateAddress.bind(this)} type="text"/></label>
+      {<br></br>}
+      <label>Bild: <input onChange={this.updateImage.bind(this)} type="text"/></label>
+      {<br></br>}
+
+      <button onClick={this.saveKontakt.bind(this)}>Speichern</button>
+      <button onClick={this.cancel.bind(this)}>Abbrechen</button>
+      </React.Fragment>
+      );
+    }
+    else
+    {
+      return (
+        //Fragment Anzeigen um Abzugrenzen
+      <React.Fragment>
+      <h1>Kontakte</h1>
+
+      <button onClick={this.addKontakt.bind(this)}>Kontakt hinzufügen</button>
+    
+      <h2>Alle Kontakte</h2>
+      <ul>
+        {this.state.contacts.map((item, index) => {
+          return (
+            <li key={index}>
+              {<img src={item.image}></img>}
+            {<br></br>}
+              {item.name}({item.age})
+            {<br></br>}
+              {item.phone}
+            {<br></br>}
+              {item.address}
+            {<br></br>}
+              {item.email}
+            {<br></br>}
+              {<button onClick={this.editKontakt.bind(this, item.email)}>Kontakt bearbeiten</button>}
+              {<button onClick={this.delKontakt.bind(this, item.email)}>Kontakt löschen</button>}
+            </li>);
+        })}
+
+      </ul>
+      </React.Fragment>
+      );
+    }
+    }
+  }
+
+
+//Classe Aufrufn und HTML Element ändern
+ReactDOM.render(
+ <App />,
+ document.getElementById('root')
+);
+
+
+
+/*
 // ---------------------------------------------------------------------------------------------------- Array mit Kontaktobjekten
 
 var contact_deleted = false;
 var contact_make_new = false;
 var contact_edit = false;
+
 
 var contactsArray = [
 {
@@ -166,8 +359,8 @@ var ContactInfo = React.createClass({ displayName: "ContactInfo",
               break;
             }
           }
-          console.log("value of input field : "+this.state.inputfield);
-          contactsArray[index].name = this.state.inputfield;
+          console.log("value of input field : "+this.state.name);
+          contactsArray[index].name = this.state.name;
           console.log('Kontakt erfolgreich bearbeitet');
           this.setState({contactsArray: contactsArray}); // State aktualisieren
       },
@@ -176,13 +369,15 @@ var ContactInfo = React.createClass({ displayName: "ContactInfo",
         contact_edit = false;
         this.setState({contactsArray: contactsArray}); // State aktualisieren
       },
-      updateInputValue: function (evt){
+      // ----------------------------------------------------------------- Funktion: Inputfield aktualisieren
+      updateName: function (event){
         //console.log("input field updated with "+evt.target.value);
-        this.state={inputfield: evt.target.value};
+        this.setState({name: event.target.value}); // State aktualisieren
+        //this.state={inputfield: evt.target.value};
       },
   render: function () {
     // Wenn ein Kontakt gelöscht wurde, einen Hinweis anzeigen
-    if(  contact_deleted == true)
+    if(contact_deleted == true)
     {
       contact_deleted = false;
       var styles = {
@@ -205,13 +400,13 @@ var ContactInfo = React.createClass({ displayName: "ContactInfo",
   
         // Unterer Bereich
         React.createElement("section", null,
-        React.createElement("input", { className: "input_name", placeholder: "Bitte Namen eingeben", value: this.props.person.name, autoFocus: true, onChange: this.updateInputValue.bind(this.props.person.name)}, this.props.person.name),
-        React.createElement("input", { className: "input_phone", placeholder: "Bitte Telefonnummer eingeben", value: this.props.person.phone, onChange: this.updateInputValue.bind(this)}, this.props.person.phone),
-        React.createElement("input", { className: "input_email", placeholder: "Bitte eine Emailadresse eingeben", value: this.props.person.email, onChange: this.updateInputValue.bind(this)}, this.props.person.email),
-        React.createElement("input", { className: "input_address", placeholder: "Adresse eingeben", value: this.props.person.address, onChange: this.updateInputValue.bind(this)}, this.props.person.address),
-        React.createElement("input", { className: "input_image", placeholder: "Bildadresse eingeben", value: this.props.person.image, onChange: this.updateInputValue.bind(this)}, this.props.person.image),
-        React.createElement("button", { className: "cancelSave",  onClick: this.cancelSave.bind()}, "cancelSave"),
-        React.createElement("button", { className: "saveContact",  onClick: this.saveContact.bind(this, this.props.person.name)}, "saveContact"))));
+        React.createElement("input", { className: "input_name", placeholder: "Bitte Namen eingeben", value: this.props.person.name, autoFocus: true, onChange: this.updateName.bind(this)}, this.props.person.name),
+        React.createElement("input", { className: "input_phone", placeholder: "Bitte Telefonnummer eingeben", value: this.props.person.phone}, this.props.person.phone),
+        React.createElement("input", { className: "input_email", placeholder: "Bitte eine Emailadresse eingeben", value: this.props.person.email}, this.props.person.email),
+        React.createElement("input", { className: "input_address", placeholder: "Adresse eingeben", value: this.props.person.address}, this.props.person.address),
+        React.createElement("input", { className: "input_image", placeholder: "Bildadresse eingeben", value: this.props.person.image}, this.props.person.image),
+        React.createElement("button", { className: "cancelSave",  onClick: this.cancelSave.bind()}, "Abbrechen"),
+        React.createElement("button", { className: "saveContact",  onClick: this.saveContact.bind(this, this.props.person.name)}, "Kontakt speichern"))));
     }
     // Wenn keine Kontakt gelöscht wurde, jedoch ein Kontakt gewählt worden ist, diese Daten anzeigen
     else
@@ -237,4 +432,4 @@ var ContactInfo = React.createClass({ displayName: "ContactInfo",
 React.render(
 React.createElement(ContactList, null),
 document.body);
-
+*/
